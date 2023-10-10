9525b72f01c8b70a34577333322826e7d57f1cf3
marksikaundi <emmanuelmark117@gmail.com>
2023-10-10 22:01:36 +0200

    screen header btn done

--- app/index.js
+++ app/index.js
@@ -25,7 +25,7 @@ const Home = () => {
             <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
           ),
           headerRight: () => (
-            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
           ),
           headerTitle: "",
         }}
