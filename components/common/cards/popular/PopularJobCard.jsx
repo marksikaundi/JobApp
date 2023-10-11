64ee58331562ca268af98b3d986475cad718ef1d
marksikaundi <emmanuelmark117@gmail.com>
2023-10-11 06:28:50 +0200

    done with pjc

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -3,6 +3,8 @@ import { View, Text, TouchableOpacity, Image } from "react-native";
 
 import styles from "./popularjobcard.style";
 
+import { checkImageURL } from "../../../../utils";
+
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
     <TouchableOpacity
@@ -11,8 +13,8 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
     >
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image
-          source={{ uri: item.employer_logo }}
-          resizeMode="contain"
+          source={{ uri: checkImageURL(item.employer_logo) ? item.employer_logo :
+          "https://img.freepik.com/free-vector/man-search-hiring-job-online-from-laptop_1150-52728.jpg"}}
           style={styles.logoImage}
         />
       </TouchableOpacity>
@@ -20,9 +22,10 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
         {item.employer_name}
       </Text>
       <View style={styles.infoContainer}>
-        <Text style={styles.jobName(selectedJob, item)}>
-          
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
         </Text>
+        <Text style={styles.location}>{item.job_country}</Text>
       </View>
     </TouchableOpacity>
   );
---
+++ utils/index.js
@@ -0,0 +1,6 @@
+export const checkImageURL = (url) => {
+    if (!url) return false
+    else {
+        const pattern = new RegExp('^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|svg|webp)$', 'i')
+    }
+}
